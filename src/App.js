6504b5e5d008fa498770badb4ee79c5c38528163
Nastya <nshpv@icloud.comgit config --global user.email n.shpv@icloud.com'git config --global user.email n.shpv@icloud.com>
2024-04-06 23:44:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Chart } from "react-google-charts";
+
+export const data = [
+    ["Task", "Hours per Day"],
+    ["ты не верь слезам", 9],
+    ["все вернется", 6],
+    ["после долгих ночей", 4],
+    ["былью сладкий сон", 2],
+    ["обернется", 7],
+];
+
+export const options = {
+    title: "Ты не верь слезам",
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Chart chartType="PieChart" data={data} options={options} width={"100%"} height={"400px"} />;
 }
 
 export default App;
